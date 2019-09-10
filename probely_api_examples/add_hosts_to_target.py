6801a1aee8297b81884e4b93ee9c84c9d0f3e56c
Hugo Castilho <hugo@castilho.me>
2019-09-10 15:15:20 +0100

    Example script adding hosts to an existing target

---
+++ probely_api_examples/add_hosts_to_target.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+"""
+Add hosts to an existing target
+
+This action may only be performed by users with the required permissions,
+target API keys will no be able to create targets.
+
+"""
+import requests
+from urllib.parse import urljoin
+
+token = input("API Token: ")
+target_id = input("Target ID: ")
+
+headers = {
+    'Authorization': "JWT {}".format(token),
+    'Content-Type': "application/json",
+}
+api_base_url = "https://api.stg.probely.com"
+endpoint = urljoin(api_base_url, "targets/{target_id}/assets/")
+
+response = requests.post(
+    endpoint.format(target_id=target_id),
+    headers=headers,
+    json={'host': 'example.com'})
+
+print(response.request.headers)
+
+if response.status_code == 200:
+    print('\nSUCCESS')
+
+else:
+    print('\n[%s]\n%s' %(response.status_code, response.text))
